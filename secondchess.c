f8404cd88b4eda8e21a3251598d099bbeec4d835
emdio <uc3709@yahoo.es>
2018-11-04 19:31:08 +0100

    fix output of calling perft. nps -> Mnps

--- secondchess.c
+++ secondchess.c
@@ -1976,7 +1976,7 @@ main ()
 	  printf ("nodes = %llu\n", count);
 	  printf ("time = %.2f s\n", t);
           double Mnps = (count/t)/1000000;
-          printf ("nodes/second = %.2f nps\n", Mnps);
+          printf ("MegaNodes/second = %.2f nps\n", Mnps);
 	  continue;
 	}
       if (!strcmp (s, "quit"))
